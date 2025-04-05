50809bd08487e94e830601593aeec2d6a8dce7e2
Lyhengsun <101932930+Lyhengsun@users.noreply.github.com>
2025-04-05 14:16:15 +0700

    fixed bean annotation for password encoder

--- src/main/java/com/example/gamified_habit_tracker/config/AppConfig.java
+++ src/main/java/com/example/gamified_habit_tracker/config/AppConfig.java
@@ -1,14 +1,13 @@
 package com.example.gamified_habit_tracker.config;
 
-import java.beans.BeanProperty;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
-    @BeanProperty
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
